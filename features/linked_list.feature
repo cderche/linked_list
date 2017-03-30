13bcc8ca001ac5638cee5909974f24dceb611a04
cderche <cyrille@imac-de-gerard.home>
2017-03-30 23:46:56 +0200

    added features. not working.

---
+++ features/linked_list.feature
@@ -0,0 +1,33 @@
+Feature: Linked List Object
+
+Scenario: Creating a linked list
+  Given a new list
+  Then the list should have 1 objects
+  And the head should be the current object
+  And the tail should be the current object
+  And the object should be first
+  And the object should be last
+  And the first object should not have a previous
+  And the last object should not have a next
+
+Scenario: Adding a object to a linked list
+  Given a list of 1 objects
+  When I add a new object
+  Then the list should have 2 objects
+  And the head should be the same of all objects
+  And the tail should be the same of all objects
+  And the head should be the first object
+  And the tail should be the last object
+  And the new object should be last
+
+Scenario: Validating a linked list
+  Given a list of 3 objects
+  Then the list should have 3 objects
+  And the head should be the first object
+  And the tail should be the last object
+  And object 1 should be next to object 0
+  And object 0 should be previous to object 1
+  And object 2 should be next to object 1
+  And object 1 should be previous to object 2
+  And the first object should not have a previous
+  And the last object should not have a next
---
+++ features/step_definitions/linked_list.rb
@@ -0,0 +1,100 @@
+Given(/^a new list$/) do
+  expect { @head = Node.create }.to change(Node, :count).by(1)
+  @head.reload
+end
+
+Given(/^a list of (\d+) objects$/) do |arg1|
+  expect { @head = Node.create }.to change(Node, :count).by(1)
+
+  x = arg1.to_i - 1
+  x.times do |i|
+    expect { @tail = Node.create }.to change(Node, :count).by(1)
+    expect { @head.add(@tail) }.to change(@head.siblings, :count).by(1)
+    @tail.reload
+  end
+  @head.reload
+end
+
+When(/^I add a new object$/) do
+  expect { @tail = Node.create }.to change(Node, :count).by(1)
+  expect { @head.add(@tail) }.to change(@head.reload.siblings, :count).by(1)
+  @head.reload
+  @tail.reload
+end
+
+Then(/^the list should have (\d+) objects$/) do |arg1|
+  expect(@head.siblings.count).to eq(arg1.to_i)
+end
+
+Then(/^the head should be the current object$/) do
+  expect(@head.head).to eq(@head)
+end
+
+Then(/^the tail should be the current object$/) do
+  @head.reload
+  expect(@head.tail).to eq(@head)
+end
+
+Then(/^the object should be first$/) do
+  expect(@head.first?).to be_truthy
+end
+
+Then(/^the head should be the same of all objects$/) do
+  @head.siblings.each do |obj|
+    expect(obj.head_id).to eq(@head.head_id)
+  end
+end
+
+Then(/^the tail should be the same of all objects$/) do
+  @head.siblings.each do |obj|
+    expect(obj.tail).to eq(@head.tail)
+  end
+end
+
+Then(/^the head should be the first object$/) do
+  expect(@head.head.first?).to be_truthy
+end
+
+Then(/^the tail should be the last object$/) do
+  expect(@head.tail.last?).to be_truthy
+end
+
+Then(/^the object should be last$/) do
+  expect(@head.last?).to be_truthy
+end
+
+Then(/^the new object should be last$/) do
+  expect(@tail.last?).to be_truthy
+end
+
+Then(/^object (\d+) should be next to object (\d+)$/) do |arg1, arg2|
+  a = @head
+  x = arg1.to_i
+  x.times { a = a.next }
+
+  b = @head
+  y = arg2.to_i
+  y.times { b = b.next }
+
+  expect(a).to eq(b.next)
+end
+
+Then(/^object (\d+) should be previous to object (\d+)$/) do |arg1, arg2|
+  a = @head
+  x = arg1.to_i
+  x.times { a = a.next }
+
+  b = @head
+  y = arg2.to_i
+  y.times { b = b.next }
+
+  expect(a).to eq(b.previous)
+end
+
+Then(/^the first object should not have a previous$/) do
+  expect(@head.head.previous?).to be_falsy
+end
+
+Then(/^the last object should not have a next$/) do
+  expect(@head.tail.next?).to be_falsy
+end
